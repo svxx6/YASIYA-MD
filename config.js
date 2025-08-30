a64ce9b162356adefdc5fde3ec42b0c3e8026cb1
svxx6 <sarithvimukthix@gmail.com>
2025-08-30 07:27:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~atB2SCJa#rQy0_CNzIu0PzFGXiQ1GTSpJGRKXM7TkF_PfaCIGiNc'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~7gZGAZaK#kTwHLARb2Qonx-z62W-CuGYh8Xm7NM08TGrlp2I3BJA'
 };
